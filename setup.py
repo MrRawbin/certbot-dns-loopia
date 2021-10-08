6af1c906f62980f02dc790d162c82c778340b5e8
kmpm <me@kmpm.se>
2021-10-08 13:46:03 +0200

    Add files to create a snap package

--- setup.py
+++ setup.py
@@ -1,52 +1,67 @@
+import os
 from setuptools import setup
 
 
-try:
-    long_desc = open("README.rst").read()
-except FileNotFoundError:
-    print("Skipping README.rst for long description as it was not found")
-    long_desc = None
+version = '1.0.0'
+cb_required = '1.8.0'
+
+install_requires = [
+    'loopialib>=0.2.0',
+    'zope.interface>=4.4.0',
+]
+
+
+if not os.environ.get('SNAP_BUILD'):
+    install_requires.extend([
+        f'acme>={cb_required}',
+        f'certbot>={cb_required}',
+    ])
+
+
+if os.environ.get('SNAP_BUILD'):
+    install_requires.append('packaging')
+
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(BASE_PATH, 'README.rst')) as f:
+    long_desc = f.read()
+
 
 
 setup(
-    name="certbot-dns-loopia",
-    version="1.0.0",
-    description="Loopia DNS authentication plugin for Certbot",
+    name='certbot-dns-loopia',
+    version=version,
+    description='Loopia DNS authentication plugin for Certbot',
     long_description=long_desc,
-    license="BSD",
-    author="Andreas Runfalk",
-    author_email="andreas@runfalk.se",
-    url="https://www.github.com/runfalk/certbot-loopia",
-    py_modules=["certbot_dns_loopia"],
-    install_requires=[
-        "acme>=1.8.0",
-        "certbot>=1.7.0",
-        "loopialib>=0.2.0",
-        "zope.interface>=4.4.0",
-    ],
+    license='BSD',
+    author='Andreas Runfalk',
+    author_email='andreas@runfalk.se',
+    url='https://www.github.com/runfalk/certbot-loopia',
+    py_modules=['certbot_dns_loopia'],
+    install_requires=install_requires,
     extras_require={
-        "dev": [
-            "pytest",
-            "wheel",
+        'dev': [
+            'pytest',
+            'wheel',
         ],
     },
     entry_points={
-        "certbot.plugins": [
-            "dns-loopia = certbot_dns_loopia:LoopiaAuthenticator",
+        'certbot.plugins': [
+            'dns-loopia = certbot_dns_loopia:LoopiaAuthenticator',
         ],
     },
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Environment :: Plugins",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Internet :: Name Service (DNS)",
-        "Topic :: System :: Systems Administration",
-        "Topic :: Utilities",
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: Name Service (DNS)',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
     ],
 )
